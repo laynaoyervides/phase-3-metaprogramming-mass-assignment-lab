d034223a9931e947ad3d5ba0615eabefb673741e
Layna Oyervides <laynaoyervides@gmail.com>
2022-02-24 18:24:20 -0700

    Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
-class Person
-  # your code here
+class Person  
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
